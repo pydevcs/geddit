235e84692dc924ab2c535265dbc4e0ff5f6e2457
Casey <casey@iair.attlocal.net>
2016-10-08 02:00:26 -0700

    added unauth r/all view

--- static/geddit.js
+++ static/geddit.js
@@ -168,7 +168,6 @@ function checkAuth(permalink) {
 	        else if (permalink[0] == "/") {
 		        url = "https://www.reddit.com" + permalink;
 	        }
-	        console.log(url);
             var promise = $.ajax({
                 url: url,
                 type: "GET",
