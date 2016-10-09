b700962373fa3e768b0416718d6eb4c4915a616f
Casey <casey@iair.attlocal.net>
2016-10-08 22:34:28 -0700

    testing new features :]

--- static/geddit.js
+++ static/geddit.js
@@ -138,7 +138,7 @@ function refresh(endpoint) {
     });
 }
 
-function checkAuth(permalink) {
+	function checkAuth(permalink) {
     var token=getCookie("token");
     var str_tst = permalink.includes(".json?limit=50");
     if (!str_tst) {
@@ -229,8 +229,8 @@ function renderContent(json, endpoint) {
 	            return;
 	        }   
 	    }
-        //var timeAgo = moment.unix(ob.data.created_utc).fromNow(false);   //false includes "ago"
-        var postdate = moment.unix(ob.data.created_utc).format("MMM D");
+        var timeAgo = moment.unix(ob.data.created_utc).fromNow(false);   //false includes "ago"
+        //var postdate = moment.unix(ob.data.created_utc).format("MMM D");
         var post = "&lt;div class='mail-item' data-id='" + ob.data.name + "' data-dir='" + ob.data.likes + "'&gt;" +
         "&lt;img class='box vote' src='" + box(ob.data.likes) + "' width='14px' height='14px'&gt;" +
         "&lt;img class='star vote' src='" + star(ob.data.likes) + "' width='14px' height='13px'&gt;" +
@@ -240,7 +240,7 @@ function renderContent(json, endpoint) {
         ob.data.title + "&lt;/div&gt;&lt;/a&gt;" + 
         nsfwTag(ob.data.over_18) +
         "&lt;a class='mail-date' href='https://reddit.com" + ob.data.permalink +"' &gt;" +
-        postdate + "&lt;/a&gt;&lt;/div&gt;";
+        timeAgo + "&lt;/a&gt;&lt;/div&gt;";
         var rendered_link = $("<div />").html(post).text();
         main_list += rendered_link;
     });
