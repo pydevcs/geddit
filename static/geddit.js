6937476ef121141604806af869193c5c8a3d2d45
Casey <casey@iair.attlocal.net>
2016-10-10 21:15:03 -0700

    testing new features :]

--- static/geddit.js
+++ static/geddit.js
@@ -246,9 +246,10 @@ function renderContent(json, endpoint) {
 
 function selfTxt(post) {
     if (post != "") {
-	    post = post.replace("&lt;", "");
-	    post = post.replace("&gt;", "");
-	    post = post.replace("↵", "");
+
+        post = post.replace('&lt;', '<', 'gm')
+                   .replace('&gt;', '>', 'gm');
+	    //post = post.replace("↵", "");
 	    console.log(post);
         return "&lt;span&gt; - " + post + "&lt;/span&gt;";  
     } else {
