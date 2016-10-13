db88f9bc1065153d5395440212c4d3b41fe26fe1
pydevcs <pydevcs@gmail.com>
2016-10-12 17:10:15 -0700

    testing new features :]

--- static/geddit.js
+++ static/geddit.js
@@ -169,7 +169,7 @@ function geddit(token){
     var url;
     if (!token) {
 	    url = "https://www.reddit.com";
-	    if (endpoint == false) { endpoint = "/r/all.json?limit=50"; }
+	    if (endpoint == false || endpoint == "") { endpoint = "/r/all.json?limit=50"; }
     } else {
 	    url = "https://oauth.reddit.com";
 	    //if (endpoint == "") { endpoint = "/.json?limit=50"; }
